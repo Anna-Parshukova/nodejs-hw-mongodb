35d3fc95774d9a67e0b63afa8e98d828aa2030ac
Anna Parshukova <annpaarshukova041@gmail.com>
2024-11-14 19:58:11 +0100

    summary

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,20 +1,20 @@
-import {Router} from 'express';
+import { Router } from 'express';
 import { getContactById, getContacts } from '../services/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.get('/contacts', async (_, res) => {
+router.get('/contacts', ctrlWrapper(async (_, res) => {
     const data = await getContacts();
     res.json({
       status: 200,
       message: 'Successfully found contacts!',
       data,
     });
-  });
+  })
+);
 
-
-
-  router.get("/contacts/:_id", async (req, res) => {
+router.get("/contacts/:_id", ctrlWrapper(async (req, res) => {
     const { _id } = req.params;
     const data = await getContactById(_id);
 
@@ -30,7 +30,7 @@ router.get('/contacts', async (_, res) => {
       message: 'Successfully found contact!',
       data,
     });
-  });
-
+  })
+);
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
 import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 const PORT = Number(env('PORT', 4001));
 
@@ -10,7 +12,6 @@ const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-
   app.use(cors());
 
   const logger = pino({
@@ -21,21 +22,14 @@ const setupServer = () => {
 
   app.use(logger);
 
-  app.use(contactsRouter);
+  
+  app.use('/api', contactsRouter);
 
 
-  app.use((req, res) => {
-    res.status(404).json({
-      message: `${req.url} not found`,
-    });
-  });
+  app.use(notFoundHandler);
 
 
-  app.use((error, req, res)=> {
-    res.status(500).json({
-        message: error.message,
-    });
-});
+  app.use(errorHandler);
 
   app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 };
