795721e97fab0c4a162192db5122af1a13108a15
Anna Parshukova <annpaarshukova041@gmail.com>
2024-11-15 11:48:05 +0100

    Update contacts.js

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,36 +1,84 @@
 import { Router } from 'express';
-import { getContactById, getContacts } from '../services/contacts.js';
+import ContactsCollection from '../db/contactModel.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
+// Отримання всіх контактів
 router.get('/contacts', ctrlWrapper(async (_, res) => {
-    const data = await getContacts();
+    const data = await ContactsCollection.find();
     res.json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data,
+        status: 200,
+        message: 'Successfully found contacts!',
+        data,
     });
-  })
-);
+}));
 
-router.get("/contacts/:_id", ctrlWrapper(async (req, res) => {
+// Отримання конкретного контакту за ID
+router.get('/contacts/:_id', ctrlWrapper(async (req, res) => {
     const { _id } = req.params;
-    const data = await getContactById(_id);
+    const data = await ContactsCollection.findById(_id);
 
     if (!data) {
-      return res.status(404).json({
-        status: 404,
-        message: `Contact with id=${_id} not found!`,
-      });
+        return res.status(404).json({
+            status: 404,
+            message: `Contact with id=${_id} not found!`,
+        });
     }
 
     res.json({
-      status: 200,
-      message: 'Successfully found contact!',
-      data,
+        status: 200,
+        message: 'Successfully found contact!',
+        data,
     });
-  })
-);
+}));
+
+// Додавання нового контакту
+router.post('/contacts', ctrlWrapper(async (req, res) => {
+    const data = await ContactsCollection.create(req.body);
+    res.status(201).json({
+        status: 201,
+        message: 'Contact successfully created!',
+        data,
+    });
+}));
+
+// Оновлення існуючого контакту
+router.patch('/contacts/:_id', ctrlWrapper(async (req, res) => {
+    const { _id } = req.params;
+    const updatedContact = await ContactsCollection.findByIdAndUpdate(_id, req.body, { new: true });
+
+    if (!updatedContact) {
+        return res.status(404).json({
+            status: 404,
+            message: `Contact with id=${_id} not found!`,
+        });
+    }
+
+    res.json({
+        status: 200,
+        message: 'Contact successfully updated!',
+        data: updatedContact,
+    });
+}));
+
+// Видалення контакту
+router.delete('/contacts/:_id', ctrlWrapper(async (req, res) => {
+    const { _id } = req.params;
+    const deletedContact = await ContactsCollection.findByIdAndDelete(_id);
+
+    if (!deletedContact) {
+        return res.status(404).json({
+            status: 404,
+            message: `Contact with id=${_id} not found!`,
+        });
+    }
+
+    res.json({
+        status: 200,
+        message: 'Contact successfully deleted!',
+        data: deletedContact,
+    });
+}));
 
 export default router;
