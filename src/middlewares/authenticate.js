619b49221b03455fa8130def6b6c6dbb68bdeab2
Anna Parshukova <annpaarshukova041@gmail.com>
2025-01-21 21:39:27 +0200

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,44 +1,54 @@
+import path from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
 import createHttpError from 'http-errors';
-import { SessionCollection } from '../db/models/session.js';
-import { UsersCollection } from '../db/models/user.js';
+
+// Обчислення абсолютного шляху
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Перетворення шляху у формат file://
+const sessionPath = pathToFileURL(path.resolve(__dirname, '../db/models/session.js')).href;
+const userPath = pathToFileURL(path.resolve(__dirname, '../db/models/user.js')).href;
+
+// Динамічний імпорт модулів
+const { SessionCollection } = await import(sessionPath);
+const { UsersCollection } = await import(userPath);
 
 export const authenticate = async (req, res, next) => {
-  const auhtHeader = req.get('Authorization');
+  try {
+    const authHeader = req.get('Authorization');
 
-  if (!auhtHeader) {
-    next(createHttpError(401, 'Please, provide Authorization header'));
-    return;
+    if (!authHeader) {
+      throw createHttpError(401, 'Please, provide Authorization header');
+    }
+
+    const [bearer, token] = authHeader.split(' ');
+
+    if (bearer !== 'Bearer' || !token) {
+      throw createHttpError(401, 'Auth header should be of type Bearer');
+    }
+
+    const session = await SessionCollection.findOne({ accessToken: token });
+
+    if (!session) {
+      throw createHttpError(401, 'Session not found');
+    }
+
+    const isExpiredToken = new Date() > new Date(session.accessTokenValidUntil);
+
+    if (isExpiredToken) {
+      throw createHttpError(401, 'Access token expired');
+    }
+
+    const user = await UsersCollection.findOne({ _id: session.userId });
+
+    if (!user) {
+      throw createHttpError(401, 'User not found');
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  const [bearer, token] = auhtHeader.split(' ');
-
-  if (bearer !== 'Bearer' || !token) {
-    next(createHttpError(401, 'Auth header should be of type Bearer'));
-    return;
-  }
-
-  const session = await SessionCollection.findOne({ accessToken: token });
-
-  if (!session) {
-    next(createHttpError(401, 'Session not found'));
-    return;
-  }
-
-  const isExpiredToken = new Date() > new Date(session.accessTokenValidUntil);
-
-  if (isExpiredToken) {
-    next(createHttpError(401, 'Access token expired'));
-    return;
-  }
-
-  const user = await UsersCollection.findOne({ _id: session.userId });
-
-  if (!user) {
-    next(createHttpError(401, 'User not found'));
-    return;
-  }
-
-  req.user = user;
-
-  next();
 };
