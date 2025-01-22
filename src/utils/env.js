8e578ace47ba71e955f9d1e63ac40557389e4728
Anna Parshukova <annpaarshukova041@gmail.com>
2025-01-22 22:37:16 +0200

    Update env.js

--- src/utils/env.js
+++ src/utils/env.js
@@ -1,14 +1,17 @@
-
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 export function env(name, defaultValue) {
   const value = process.env[name];
+  if (value !== undefined) {
+    return value;
+  }
 
-  if (value) return value;
+  if (defaultValue !== undefined) {
+    return defaultValue;
+  }
 
-  if (defaultValue) return defaultValue;
 
-  throw new Error(`Missing: process.env['${name}'].`);
+  throw new Error(`Missing required environment variable: '${name}'`);
 }
